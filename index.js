daadf7ef8451fcb85412bcd3bead514d4afaa7f6
XuHuan <wongxuhuan@gmail.com>
2023-07-09 22:31:39 +0800

    hi

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://xuhuan:xuhuan01234@cluster0.7krsk3h.mongodb.net/?retryWrites=true&w=majority"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // const user = await client.db("Week5").collection("idk").find({"username": "IVE"}).toArray();
+    const user = await client.db("Week5").collection("idk").insertOne({"username":"Chua2", "password":"chua3182"})
+    console.log(user);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 let dbUsers = [
   {
       username: "Wong",
@@ -126,4 +152,4 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-console.log(login("Wong", "password"))
+// console.log(login("Wong", "password"))
